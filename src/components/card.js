ddb8e1d014681340a596f43c27685084c256c404
sethmchris <sethmchris@gmail.com>
2018-07-10 18:18:59 -0600

    Created content component to generate story

--- src/components/card.js
+++ src/components/card.js
@@ -1,76 +1,68 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 import Input from "./input";
+import Content from "./content";
 
 class Card extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
-      color: "",
-      pluralNoun: "",
-      adjectiveOne: "",
-      celebOne: ""
-    };
+      color: '',
+      pluralNoun: '',
+      adjectiveOne: '',
+      celebOne: '',
+      adjectiveTwo: '',
+      nounOne: '',
+      numberOne: '',
+      numberTwo: '',
+      nounTwo: '',
+      adjectiveThree: '',
+      celebTwo: '',
+      celebThree: '',
+      adjectiveFour: '',
+      nounThree: '',
+      celebFour: '',
+      adjectiveFive: '',
+      contentVisible: false
+  }
 
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleInputChange(event) {
     this.setState({ [event.target.name]: event.target.value });
-    console.log(this.state);
   }
 
   render() {
     const inputData = [
-      { title: "Color", state: this.state.color, name: "color" },
-      {
-        title: "Plural Noun",
-        state: this.state.pluralNoun,
-        name: "pluralNoun"
-      },
-      {
-        title: "Adjective",
-        state: this.state.adjectiveOne,
-        name: "adjectiveOne"
-      },
-      { title: "Celberity", state: this.state.celebOne, name: "celebOne" },
+      {title: 'Color', state: this.state.color, name: 'color'},
+      {title: 'Plural Noun', state: this.state.pluralNoun, name: 'pluralNoun'},
+      {title: 'Adjective', state: this.state.adjectiveOne, name: 'adjectiveOne'},
+      {title: 'Celebrity', state: this.state.celebOne, name: 'celebOne'},
 
-      {
-        title: "Adjective",
-        state: this.state.adjectiveTwo,
-        name: "adjectiveTwo"
-      },
-      { title: "Noun", state: this.state.nounOne, name: "nounOne" },
-      { title: "Number", state: this.state.numberOne, name: "numberOne" },
-      { title: "Number", state: this.state.numberTwo, name: "numberTwo" },
+      {title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
+      {title: 'Noun', state: this.state.nounOne, name: 'nounOne'},
+      {title: 'Number', state: this.state.numberOne, name: 'numberOne'},
+      {title: 'Number', state: this.state.numberTwo, name: 'numberTwo'},
 
-      { title: "Noun", state: this.state.nounTwo, name: "nounTwo" },
-      {
-        title: "Adjective",
-        state: this.state.adjectiveThree,
-        name: "adjectiveThree"
-      },
-      { title: "Celebrity", state: this.state.celebTwo, name: "celebTwo" },
-      { title: "Celebrity", state: this.state.celebThree, name: "celebThree" },
+      {title: 'Noun', state: this.state.nounTwo, name: 'nounTwo'},
+      {title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree'},
+      {title: 'Celebrity', state: this.state.celebTwo, name: 'celebTwo'},
+      {title: 'Celebrity', state: this.state.celebThree, name: 'celebThree'},
 
-      {
-        title: "Adjective",
-        state: this.state.adjectiveFour,
-        name: "adjectiveFour"
-      },
-      { title: "Noun", state: this.state.nounThree, name: "nounThree" },
-      { title: "Celebrity", state: this.state.celebFour, name: "celebFour" },
-      {
-        title: "Adjective",
-        state: this.state.adjectiveFive,
-        name: "adjectiveFive"
-      }
-    ];
+      {title: 'Adjective', state: this.state.adjectiveFour, name: 'adjectiveFour'},
+      {title: 'Noun', state: this.state.nounThree, name: 'nounThree'},
+      {title: 'Celebrity', state: this.state.celebFour, name: 'celebFour'},
+      {title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive'},
+  ]
 
     return (
       <div className="card">
-        {inputData.map(data => Input(data, this.handleInputChange))}
+        {
+          inputData.map(data => Input(data, this.handleInputChange))
+        }
+        <Content data={this.state} />
       </div>
     );
   }
